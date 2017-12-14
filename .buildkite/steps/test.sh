48e79c364c8ab31c3720a1b4f4a9e8dd44139310
Mike <mknox@assemblypayments.com>
2017-12-14 14:12:09 +1100

    fix shellcheck lint errors

--- .buildkite/steps/test.sh
+++ .buildkite/steps/test.sh
@@ -2,9 +2,9 @@
 # shellcheck disable=SC1117
 set -eu
 
-region=`curl -s -S http://169.254.169.254/latest/dynamic/instance-identity/document|grep region|awk -F\" '{print $4}'`
-vpc_id=$(aws ec2 describe-vpcs --filters "Name=isDefault,Values=true" --query "Vpcs[0].VpcId" --region ${region:-us-east-1} --output text)
-subnets=$(aws ec2 describe-subnets --filters "Name=vpc-id,Values=$vpc_id" --query "Subnets[*].[SubnetId,AvailabilityZone]" --region ${region:-us-east-1} --output text)
+region=$(curl -s -S http://169.254.169.254/latest/dynamic/instance-identity/document|grep region|awk -F\" '{print $4}')
+vpc_id=$(aws ec2 describe-vpcs --filters "Name=isDefault,Values=true" --query "Vpcs[0].VpcId" --region "${region:-us-east-1}" --output text)
+subnets=$(aws ec2 describe-subnets --filters "Name=vpc-id,Values=$vpc_id" --query "Subnets[*].[SubnetId,AvailabilityZone]" --region "${region:-us-east-1}" --output text)
 subnet_ids=$(awk '{print $1}' <<< "$subnets" | tr ' ' ',' | tr '\n' ',' | sed 's/,$//')
 az_ids=$(awk '{print $2}' <<< "$subnets" | tr ' ' ',' | tr '\n' ',' | sed 's/,$//')
 
