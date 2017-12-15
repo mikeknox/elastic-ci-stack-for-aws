f8221b4964ea8815c4a742648054869e0bb9e4f0
Mike <mknox@assemblypayments.com>
2017-12-15 14:22:11 +1100

    download vault directly

--- packer/scripts/install-vault.sh
+++ packer/scripts/install-vault.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -eu -o pipefail
 
+
+VAULT_VERSION=0.9.0
+
 echo "Installing vault..."
-sudo yum update -y -q
-sudo yum install -y vault
+
+sudo curl -Lf -o /tmp/vault.zip \
+	https://releases.hashicorp.com/vault/${VAULT_VERSION}/vault_${VAULT_VERSION}_linux_amd64.zip
+cd /tmp
+sudo unzip /tmp/vault.zip
+sudo mv vault /usr/bin/vault
